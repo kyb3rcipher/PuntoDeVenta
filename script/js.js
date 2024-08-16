c10eb73bd2b84f5edb2f0c35e49808375067bacb
Kyb3r Cipher <me@kyb3rcipher.com>
2024-08-15 18:46:05 -0600

    Move JS to OOP

--- script/js.js
+++ script/js.js
@@ -1,77 +1,88 @@
-const products = document.getElementsByClassName('product');
-const ticket = document.getElementById('ticket'),
-cart = document.getElementById('cart'), cartTbody = cart.querySelector('tbody');
+class Product {
+    constructor(product) {
+        this.product = product;
 
-function actualProducts() {
-    return cartTbody.querySelectorAll('tr');
+        this.name = product.querySelector('h4').innerText;
+        this.price = parseFloat(product.querySelector('p').innerText.replace('$', ''));
+
+        this.product.addEventListener('click', this.addToCart.bind(this));
+    }
+
+    addToCart() {
+        Cart.addProduct(this);
+    }
 }
 
-Array.from(products).forEach(product => {
-    product.addEventListener('click', () => {
-        var actualProductsNumber = actualProducts();
+class Cart {
+    static cartTbody = document.getElementById('cart').querySelector('tbody');
+    static total = document.getElementById('total');
 
-        const name = product.getElementsByTagName('h4')[0].innerText,
-        price = product.getElementsByTagName('p')[0].innerText.replace('$', '');
-        var existingProduct = false;
+    static addProduct(product) {
+        const existingProduct = Array.from(Cart.cartTbody.querySelectorAll('tr')).find(row => row.querySelector('td').innerText === product.name);
         
-        if (actualProductsNumber.length > 0) {
-            actualProductsNumber.forEach(product => {
-                if (product.querySelector('td').innerText === name) {
-                    existingProduct = product;
-                    return;
-                }
-            });
-        }
-
         if (!existingProduct) {
             const productCart = document.createElement('tr');
             productCart.classList.add('product-sell');
             productCart.innerHTML = `
-                <td>${name}</td>
+                <td>${product.name}</td>
                 <td>
                     <div class="product-sell-quantity-controls">
-                        <button class="rounded-button" onclick="productSellQuantityChange(this, 'remove')"><i class="fa-solid fa-minus"></i></button>
+                        <button class="rounded-button" onclick="Cart.productSellQuantityChange(this, 'remove')"><i class="fa-solid fa-minus"></i></button>
                         <span class="product-sell-quantity">x1</span>
-                        <button class="rounded-button" onclick="productSellQuantityChange(this, 'add')"><i class="fa-solid fa-plus"></i></button>
+                        <button class="rounded-button" onclick="Cart.productSellQuantityChange(this, 'add')"><i class="fa-solid fa-plus"></i></button>
                     </div>
                 </td>
-                <td>$${price}</td>
-                <td><button class="product-sell-remove" onclick="productSellRemove(this)"><i class="fa-solid fa-trash"></i></button></td>
+                <td>$${product.price}</td>
+                <td><button class="product-sell-remove" onclick="Cart.productSellRemove(this)"><i class="fa-solid fa-trash"></i></button></td>
             `;
-            cartTbody.appendChild(productCart);
+            
+            this.cartTbody.appendChild(productCart);
         } else {
-            productSellQuantityChange(existingProduct.querySelector('.product-sell-quantity-controls'), 'add');
+            this.productSellQuantityChange(existingProduct.querySelector('.product-sell-quantity-controls'), 'add');
         }
-        
-        updateTotal();
-    });
-});
 
-function productSellQuantityChange(button, type) {
-    const productSellQuantityControls = button.parentElement;
-    let quantity = productSellQuantityControls.getElementsByTagName('span')[0], quantityNumber = quantity.innerText.replace('x', '');
+        this.updateTotal();
+    }
+
+    static productSellQuantityChange(button, type) {
+        const productSellQuantityControls = button.parentElement;
+        let quantity = productSellQuantityControls.getElementsByTagName('span')[0], quantityNumber = quantity.innerText.replace('x', '');
+        
+        if (type == 'add') {
+            quantity.innerText = 'x' + ++quantityNumber;
+        } else {
+            if (quantityNumber > 1) {
+                quantity.innerText = 'x' + --quantityNumber;
+            } else if (confirm("¿Quieres eliminar este producto?")) {
+                this.productSellRemove(button);
+            }
+        }
+
+        this.updateTotal();
+    }
+
+    static productSellRemove(button) {
+        button.closest('tr').remove();
+        this.updateTotal();
+    }
+
+    static updateTotal() {
+        const actualSellProducts = this.cartTbody.querySelectorAll('tr');
+        let totalToPay = 0;
+
+        actualSellProducts.forEach(product => {
+            var price = parseFloat(product.querySelectorAll('td')[2].innerText.replace('$', '')), quantity = parseInt(product.querySelector('.product-sell-quantity').innerText.replace('x', ''));
+            totalToPay += price * quantity;
+            
+        });
     
-    if (type == 'add') {
-        quantity.innerText = 'x' + ++quantityNumber;
-    } else {
-        quantity.innerText = quantityNumber > 1 ? 'x' + --quantityNumber : confirm("Quieres eliminar este producto?") ? productSellRemove(productSellQuantityControls) : 'x' + quantityNumber;
+        this.total.innerHTML = `<i class="fa-solid fa-cash-register"></i> Vender - $${totalToPay.toFixed(2)}`;
     }
 }
 
-function productSellRemove(productSellQuantityControls) {
-    productSellQuantityControls.parentElement.parentElement.remove();
-    updateTotal();
-}
-
-function updateTotal() {
-    const actualSellProducts = actualProducts(), total = document.getElementById('total');
-    var totalToPay = 0;
-
-    actualSellProducts.forEach(product => {
-        var price = parseFloat(product.querySelectorAll('td')[2].innerText.replace('$', '')), quantity = parseInt(product.querySelector('.product-sell-quantity').innerText.replace('x', ''));
-        totalToPay += price * quantity;
-        
-    });
-
-    total.innerHTML = `<i class="fa-solid fa-cash-register"></i> Vender - $${totalToPay.toFixed(2)}`;
-}
+// Create objets for each product and add they code
+document.addEventListener('DOMContentLoaded', () => {
+    const productElements = document.getElementsByClassName('product');
+    
+    Array.from(productElements).forEach(product => new Product(product));
+});
