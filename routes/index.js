51c6afd62a6cfd704d09299ba474482fcfe19400
Kyb3r Cipher <me@kyb3rcipher.com>
2024-08-21 17:07:02 -0600

    Add upload image in new products

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 import {Router} from 'express';
-import sqlite3 from 'sqlite3';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 import database from '../db.js';
 
+// Configure router and database
 const router = Router();
 const db = database.abrir();
+
+// Configure multer for files upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const dir = 'resources/images/products/';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+        const nombre = req.body.nombre || 'producto';
+        const ext = path.extname(file.originalname);
+
+        // Convertir el nombre a minúsculas y reemplazar espacios con guiones
+        const sanitizedFileName = nombre.toLowerCase().replace(/\s+/g, '-') + ext;
+
+        cb(null, sanitizedFileName);
+    }
+});
+const upload = multer({ storage });
 
 router.get('/', (req, res) => {
     db.all('SELECT * FROM productos', [], (err, rows) => {
@@ -28,10 +52,10 @@ router.get('/productos', (req, res) => {
 router.get('/productos/crear', (req, res) => {
     res.render('products/crear');
 });
-router.post('/productos/crear', (req, res) => {
+router.post('/productos/crear', upload.single('imagen'), (req, res) => {
     const { nombre, precio, proveedor, tipo, codigo } = req.body;
     const codigosArray = JSON.stringify(codigo);
-    
+
     db.run('INSERT INTO productos (nombre, precio, tipo, codigo, proveedor) VALUES (?, ?, ?, ?, ?)', [nombre, precio, tipo, codigosArray, proveedor], (err) => {
         if (err) {
             console.error('Error al guardar el producto', err.message);
