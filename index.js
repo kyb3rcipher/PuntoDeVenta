6502fc05db314cd961b1272b08589984a6f667fa
srdavo <davoluis11@gmail.com>
2025-11-23 12:08:14 -0700

    Minimal changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
+// App Settings
 const app = express();
 
+/********** Routes **********/ 
 app.get('/', (req, res) => {
     res.send(`<h1>Hello Gael</h1>`)
 })
+
 
 app.listen(3000);
